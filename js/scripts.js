2508b9dddb1e23004a7526a38bf1d044f95ba637
TayNguyen32 <75840183+TayNguyen32@users.noreply.github.com>
2021-02-27 00:40:16 -0800

    Added new bat and animation added moving-bat and simple left to right animation with scaling

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,38 @@ gsap.fromTo(
   { opacity: 0, repeat: -1, duration: 1 },
   { opacity: 1, repeat: -1, duration: 3, yoyo: true }
 );
+
+let tl1 = gsap.timeline();
+let tl2 = gsap.timeline();
+
+// Box 1
+tl1.to("#moving-bat", { x: 700, duration: 2, delay: 3.5 });
+
+tl1.to("#moving-bat", {
+  rotation: 180,
+  scale: 0.5
+});
+
+tl1.to("#moving-bat", {
+  x: -50,
+  rotation: 180,
+  duration: 2
+});
+
+tl1.to("#moving-bat", {
+  rotation: 360
+});
+
+tl1.to("#moving-bat", {
+  x: 700,
+  duration: 1
+});
+
+tl1.to("#moving-bat", {
+  rotation: 180
+});
+
+tl1.to("#moving-bat", {
+  x: -50,
+  duration: 1
+});
